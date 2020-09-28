28ce9fe9000aea7f9c8de325e5ab009ad32b3dd9
Oleksandr Bondar <alexandr0084@gmail.com>
2020-09-28 19:34:56 +0200

    code migration

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+export class UserCv {
+        constructor() {
+
+        }
+        firstName: string;
+        lastName: string;
+        birthDay: string;
+        cityName: string;
+        roleName: string;
+        phoneNumber: string;
+        emailAdress: string;
+        description: string;
+
+        skils: {
+                groupName: string;
+                name: string;
+                value: string;
+        }[]
+
+        experiences: {
+                companyName: string;
+                roleName: string;
+                dateFrom: string;
+                dateTo: string;
+                description: string;
+                resposobility: string;
+                stackTechnology: string;
+        }[]
+        educations: {
+                universityName: string;
+                courseName: string;
+                dateFrom: string;
+                dateTo: string;
+                degreeLevel: string;
+        }[]
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'bond-cv-light-template';
+
+   userCv = new UserCv();
+
+    constructor(private httpClient: HttpClient) { }
+
+    ngOnInit() {
+      this.httpClient.get<UserCv>("http://localhost:8080/users?id=1").subscribe((data: UserCv)=>{ this.userCv = data });
+    }
+
+}
